0c5d393dd2de1abe245ce68e11e74f63d7df1284
oipul <joaolucassoaresbezerra@gmail.com>
2020-08-24 15:52:15 -0300

    arquivos

---
+++ Script.js
@@ -0,0 +1,31 @@
+const button = document.getElementById("btnCalc");
+const saida = document.getElementById("div_CalcResultsFrame");
+let imc = null;
+
+const Calcular = () => {
+    const nome = document.getElementById("nome").value;
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value;
+    if ( nome !== "" && altura !== "" && peso !== "") {
+        imc = peso /(altura * altura);
+        if (imc <= 18.5 ) {
+            saida.innerHTML = nome + "seu imc é " + imc + ", você esta a baixo do peso";
+        }
+        if ( imc > 18.5 || imc <= 24.9)  {
+            saida.innerHTML = nome + " seu imc é " + imc + ", você esta com peso saudável";
+        }
+
+        if ( imc > 25.0 || imc <= 29.9)  {
+            saida.innerHTML = nome + " seu imc é " + imc + ", você esta com sobrepeso";
+        }
+
+        if ( imc >= 30 )  {
+            saida.innerHTML = nome + " seu imc é " + imc + ", você esta com Obesidade";
+        }
+    }
+    else {
+        alert ("preencha todos os campos");
+    }
+}
+
+button.addEventListener("click" , Calcular);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Calculadora simples de IMC em JavaScirpt
+
+uma pequena atividade em uma das aulas de Front-end do senai
